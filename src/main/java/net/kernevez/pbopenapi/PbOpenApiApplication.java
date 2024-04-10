a7f6b21d0f2457315ab7bb6caee94f419353ec6d
Philippe Kernevez <philippe.kernevez@or-dynamics.com>
2024-04-10 22:58:34 +0200

    only 3.0.4

---
+++ src/main/java/net/kernevez/pbopenapi/PbOpenApiApplication.java
@@ -0,0 +1,13 @@
+package net.kernevez.pbopenapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PbOpenApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PbOpenApiApplication.class, args);
+	}
+
+}
